f947ada71c6a780fcb29bf6aa2a9fb1ba341c816
chiaweil <codenamewei@gmail.com>
2019-08-20 00:15:35 +0800

    credit card fraud code done

--- credit-card-fraud/README.md
+++ credit-card-fraud/README.md
@@ -11,25 +11,71 @@ This dataset is highly unbalanced, adding complexity to the data modelling.
 
 ## File Structure 
 ```
-CreditCardFraud 
+credit-card-fraud 
 │   
-│───App.java    
+│───src/main/java/com/codenamewei/CreditCardFraud/App.java    
 │
-└───DataPreprocessing
-│   │   Credit-Card-Data-Cleaning_0.ipynb
-│   │   Credit-Card-Data_Splitting_1.ipynb
+│───PrePostProcessing   
+│└─────DataPreprocessing
+│     │   Credit-Card-Data-Cleaning_0.ipynb
+│     │   Credit-Card-Data_Splitting_1.ipynb
 │   
-└───ResultPostProcessing
-    │   Credit-Card-Result-Visualization.ipynb
+└ ─────ResultPostProcessing
+      │   Credit-Card-Result-Visualization.ipynb
 ```
+
 **App.java:**
  
-**Credit-Card-Data-Cleaning_0.ipynb:**
+**Credit-Card-Data-Cleaning_0.ipynb:**  
+Remove first row of the data(column names) and first column of the data(Time feature).
  
-**Credit-Card-Data_Splitting_1.ipynb:**
+**Credit-Card-Data_Splitting_1.ipynb:**  
+- Normalize the total transaction column of the data
+- Split data points into feature and labels and store as .csv.
 
 **Credit-Card-Result-Visualization.ipynb:**
+Visualize the results of the testing data and plot recall-precision curve.  
 
 ## Data Sources
 Data is retrieved from https://www.kaggle.com/mlg-ulb/creditcardfraud.  
-Further processing is done to separate each data point to a file.
+Preprocessing steps below are done to obtain the current CreditCardFraud.zip data for training and testing.    
+
+(1) Drop the time column (For modelling with LSTM, time feature is not necessary)  
+(2) Drop the row with name of the columns (Time, V1, V2, V3 ...) 
+(3) Normalize the total amount transaction column into the range [-1, 1].   
+(4) Split each data point into feature(.csv) and label(.csv) and place in separate directories according to the label  
+(5) Redistribute the files into training and testing data sets.  
+
+Each data point(.csv) have a file(.csv) with the same name in the *_label.  
+For example, train_data_feature/0.csv<-->train_data_label/0.csv
+
+### Data Structure of CreditCardFraud.zip
+
+Data structure and distribution in CreditCardFraud.zip is elaborated as follows
+```
+CreditCardFraud.zip
+│
+└───train_data_feature
+│   │   0.csv
+│   │   1.csv
+│   │   ...
+│   │   99999.csv
+│   
+└───train_data_label
+│   │   0.csv
+│   │   1.csv
+│   │   ...
+│   │   99999.csv
+│   
+└───test_data_feature
+│   │   0.csv
+│   │   1.csv
+│   │   ...
+│   │   8489.csv
+│   
+└───test_data_label
+│   │   0.csv
+│   │   1.csv
+│   │   ...
+│   │   8489.csv
+```
--- credit-card-fraud/src/main/java/com/codenamewei/CreditCardFraud/App.java
+++ credit-card-fraud/src/main/java/com/codenamewei/CreditCardFraud/App.java
@@ -1,4 +1,4 @@
-package com.codenamewei.SequenceAnomalyDetection.CreditCardFraud;
+package com.codenamewei.CreditCardFraud;
 
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
@@ -17,6 +17,7 @@ import org.deeplearning4j.nn.weights.WeightInit;
 import org.deeplearning4j.ui.api.UIServer;
 import org.deeplearning4j.ui.stats.StatsListener;
 import org.deeplearning4j.ui.storage.InMemoryStatsStorage;
+import org.deeplearning4j.util.ModelSerializer;
 import org.nd4j.linalg.activations.Activation;
 import org.nd4j.linalg.api.ndarray.INDArray;
 import org.nd4j.linalg.dataset.DataSet;
@@ -41,25 +42,32 @@ import java.io.File;
  * The dataset was retrieved from https://www.kaggle.com/mlg-ulb/creditcardfraud,
  * processed and dissected into smaller dataset as preprocessing step.
  *
- * The dataset is annotated with labels.
+ * The data set is annotated with labels.
  * The features for each data point are saved in {train/test}_data_feature directory while
  * while the label is saved in {train/test}_data_label folder with the same file name.
  *
+ * Total Dataset:
+ * Non-Fraud Data: 284 315
+ * Fraud Data:     492
+ *
  * Training Data:
- *     Non-Fraud Data Size: 100000 (File Index: 0 - 99999) (inclusive)
+ *     Non-Fraud Data Size: 255 884 (File Index: 0 - 255883) (inclusive)
  * Testing Data:
- *     Non Fraud Data Size: 8000 (Index: 0 - 7999)
- *     Fraud Data Size: 490 (Index: 8000 - 8489)
+ *     Non Fraud Data Size: 28431 (Index: 255 884 - 284314)
+ *     Fraud Data Size: 492 (Index: 284315 - 284806)
+ *
+ * Instructions:
+ * 1. Download CreditCardFraud.zip file from this link
  *
  * @author ChiaWei Lim
  */
 public class App
 {
     static final int trainMinIndex = 0;
-    static final int trainMaxIndex = 99999;
+    static final int trainMaxIndex = 255883;
 
-    static final int testMinIndex = 0;
-    static final int testMaxIndex = 8489;
+    static final int testMinIndex = 255884;
+    static final int testMaxIndex = 284806;
 
     static final int seed = 123;
     static final int featureNodes = 29;
@@ -72,17 +80,18 @@ public class App
 
     public static void main(String[] args) throws Exception
     {
-        //Set the train and test data paths
-        File zipFilePath = new ClassPathResource("CreditCardFraud.zip").getFile();
+        //Unzip the downloaded file from the /resources/CreditCardFraud.zip into designated directory
+        File zipFilePath = new File("C:\\Users\\chiaw\\Downloads\\" +  "CreditCardFraud.zip");
         File baseDir = unzipFile(zipFilePath);
 
-        File trainFeaturesDir = new File(baseDir, "\\train_data_feature");
-        File trainLabelsDir = new File(baseDir, "\\train_data_label");
+        //Predefined directories in the .zip data file
+        File trainFeaturesDir = new File(baseDir, "\\train_feature");
+        File trainLabelsDir = new File(baseDir, "\\train_label");
 
-        File testFeaturesDir = new File(baseDir, "\\test_data_feature");
-        File testLabelsDir= new File(baseDir, "\\test_data_label");
+        File testFeaturesDir = new File(baseDir, "\\test_feature");
+        File testLabelsDir= new File(baseDir, "\\test_label");
 
-        //Load data and split into training and testing datasets
+        //Load data and split into training and testing data set
         Pair<SequenceRecordReader, SequenceRecordReader> rrTrain = getCreditCardDataReader(trainFeaturesDir, trainLabelsDir, trainMinIndex, trainMaxIndex);
         DataSetIterator trainIter = new SequenceRecordReaderDataSetIterator(rrTrain.getLeft(), rrTrain.getRight(),
                 miniBatchSize, numLabelClasses, false, SequenceRecordReaderDataSetIterator.AlignmentMode.ALIGN_END);
@@ -118,7 +127,6 @@ public class App
 
         // Train model.
         // It is worth to take note that the model is only trained with data of non-frauds to learn the general representation of non fraud data.
-
         System.out.println("Start Training");
 
         int nEpochs = 1;
@@ -136,34 +144,59 @@ public class App
             System.out.println("Epoch " + i + " completed");
         }
 
+        //Save model
+        File savedModelPath = new File(System.getProperty("user.home")+ "/DL4JDataDir" + "/CreditCardFraudModel.zip");
+        ModelSerializer.writeModel(model, savedModelPath, false);
 
         System.out.println("Run evaluation on test data");
-
         //Evaluate the model with data of both frauds and non-frauds
         evaluateTestData(model, testIter);
 
-        System.out.println("Program end...");
 
+        System.out.println("Program end...");
     }
 
+    /**
+     * Unzip file .zip to a designated location
+     * @param zipFilePath zip file name
+     * @return file path
+     * @throws Exception
+     */
     public static File unzipFile(File zipFilePath) throws Exception
     {
         File rootDir = new File(System.getProperty("user.home")+ "/DL4JDataDir");
-
-        if(!rootDir.exists()) rootDir.mkdir();
 
         File dataDir = new File(rootDir, "/CreditCardFraudData");
 
         if(!dataDir.exists())
         {
+            System.out.println("Unzipping data file...");
+
+            if(!rootDir.exists()) rootDir.mkdir();
+
             dataDir.mkdir();
+
             ArchiveUtils.unzipFileTo(zipFilePath.getAbsolutePath(), dataDir.getAbsolutePath());
+        }
+        else
+        {
+            System.out.println("Data already exist. Proceed");
         }
 
         return dataDir.getAbsoluteFile();
     }
 
 
+    /**
+     * Get SequenceRecordReader for input files
+     *
+     * @param featureDir features home directory path
+     * @param labelDir labels home directory path
+     * @param minIndex minimum file index (Files index should be continuous, exp: 0.csv, 1.csv ...)
+     * @param maxIndex maximum file index
+     * @return Pair of SequenceRecordReader of features and labels
+     * @throws Exception
+     */
     public static Pair<SequenceRecordReader, SequenceRecordReader> getCreditCardDataReader(File featureDir, File labelDir, int minIndex, int maxIndex) throws Exception
     {
         int skipNumLines = 0;
@@ -179,8 +212,11 @@ public class App
         return new ImmutablePair<>(features, labels);
     }
 
-
-
+    /**
+     * Evaluate model of testing data set
+     * @param model trained model
+     * @param iter DataSetIterator for testing data set
+     */
     public static void evaluateTestData(MultiLayerNetwork model, DataSetIterator iter)
     {
         int tp = 0; int tn = 0; int fp = 0; int fn = 0;
